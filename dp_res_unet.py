f4418837bf0aa2a6dccb83b94d103b88d1e3769a
Nevermoi <wlgq@hotmail.com>
2019-08-24 11:42:09 +0800

    Update dp_res_unet.py

--- dp_res_unet.py
+++ dp_res_unet.py
@@ -1,5 +1,11 @@
+"""
+credit: https://github.com/DuFanXin/deep_residual_unet
+
+import build_res_unet to make use of this module
+"""
+
 from keras.models import *
-from keras.layers import Input, Conv2D, UpSampling2D, BatchNormalization, Activation, add, concatenate
+from keras.layers import Input, Conv2D, UpSampling2D, MaxPooling2D, BatchNormalization, Activation, add, concatenate
 
 
 def res_block(x, nb_filters, strides):
@@ -9,7 +15,8 @@ def res_block(x, nb_filters, strides):
     res_path = BatchNormalization()(res_path)
     res_path = Activation(activation='relu')(res_path)
     res_path = Conv2D(filters=nb_filters[1], kernel_size=(3, 3), padding='same', strides=strides[1])(res_path)
-
+    res_path = MaxPooling2D(pool_size=(2, 2))(res_path)
+    
     shortcut = Conv2D(nb_filters[1], kernel_size=(1, 1), strides=strides[0])(x)
     shortcut = BatchNormalization()(shortcut)
 
@@ -33,10 +40,10 @@ def encoder(x):
     # first branching to decoder
     to_decoder.append(main_path)
 
-    main_path = res_block(main_path, [128, 128], [(2, 2), (1, 1)])
+    main_path = res_block(main_path, [128, 64], [(2, 2), (1, 1)])
     to_decoder.append(main_path)
 
-    main_path = res_block(main_path, [256, 256], [(2, 2), (1, 1)])
+    main_path = res_block(main_path, [256, 128], [(2, 2), (1, 1)])
     to_decoder.append(main_path)
 
     return to_decoder
@@ -45,15 +52,15 @@ def encoder(x):
 def decoder(x, from_encoder):
     main_path = UpSampling2D(size=(2, 2))(x)
     main_path = concatenate([main_path, from_encoder[2]], axis=3)
-    main_path = res_block(main_path, [256, 256], [(1, 1), (1, 1)])
+    main_path = res_block(main_path, [128, 256], [(1, 1), (1, 1)])
 
     main_path = UpSampling2D(size=(2, 2))(main_path)
     main_path = concatenate([main_path, from_encoder[1]], axis=3)
-    main_path = res_block(main_path, [128, 128], [(1, 1), (1, 1)])
+    main_path = res_block(main_path, [64, 128], [(1, 1), (1, 1)])
 
     main_path = UpSampling2D(size=(2, 2))(main_path)
     main_path = concatenate([main_path, from_encoder[0]], axis=3)
-    main_path = res_block(main_path, [64, 64], [(1, 1), (1, 1)])
+    main_path = res_block(main_path, [32, 64], [(1, 1), (1, 1)])
 
     return main_path
 
@@ -63,7 +70,7 @@ def build_res_unet(input_shape):
 
     to_decoder = encoder(inputs)
 
-    path = res_block(to_decoder[2], [512, 512], [(2, 2), (1, 1)])
+    path = res_block(to_decoder[2], [512, 256], [(2, 2), (1, 1)])
 
     path = decoder(path, from_encoder=to_decoder)
 

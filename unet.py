1928242ce542163403fd1a8edf03a4714c4bf05e
sam wang <wlgq@hotmail.com>
2019-09-28 20:03:15 +0800

    add new mean unet

--- unet.py
+++ unet.py
@@ -84,12 +84,12 @@ class UNetConvBlock(nn.Module):
         super(UNetConvBlock, self).__init__()
         block = []
 
-        block.append(nn.Conv2d(in_size, out_size, kernel_size=5, padding=int(padding)))
+        block.append(nn.Conv2d(in_size, out_size, kernel_size=3, padding=int(padding)))
         block.append(nn.ReLU())
         if batch_norm:
             block.append(nn.BatchNorm2d(out_size))
 
-        block.append(nn.Conv2d(out_size, out_size, kernel_size=5, padding=int(padding)))
+        block.append(nn.Conv2d(out_size, out_size, kernel_size=3, padding=int(padding)))
         block.append(nn.ReLU())
         if batch_norm:
             block.append(nn.BatchNorm2d(out_size))
